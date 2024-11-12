312535ba075d98e977c6fb40018fef63b2edf513
idalxdd <jubaacioly@gmail.com>
2024-11-12 17:27:05 -0300

    feat: instalação de tabelas com 7 individuos no install

--- src/main/java/br/mendonca/testemaven/services/InstallService.java
+++ src/main/java/br/mendonca/testemaven/services/InstallService.java
@@ -39,5 +39,18 @@ public class InstallService {
                 + "altura FLOAT,"
                 + "saude VARCHAR(100),"
                 + "observacoes TEXT)");
+
+				String sqlInsertExamples = "INSERT INTO relatorio_crescimento (data_registro, altura, saude, observacoes) VALUES " +
+				"(20230101, 15.5, 'Boa', 'Planta em crescimento saudável.'), " +
+				"(20230105, 18.2, 'Regular', 'Apresenta leve amarelamento nas folhas.'), " +
+				"(20230110, 20.3, 'Boa', 'Respondeu bem à adubação.'), " +
+				"(20230115, 22.0, 'Ruim', 'Folhas com pontos marrons.'), " +
+				"(20230120, 24.1, 'Boa', 'Crescimento vigoroso e folhas verdes.'), " +
+				"(20230125, 26.4, 'Boa', 'Necessita de mais água.'), " +
+				"(20230130, 28.8, 'Regular', 'Folhas secando nas pontas devido ao calor.')";
+			statement(sqlInsertExamples);			
+	
+				
     }
+	
 }
