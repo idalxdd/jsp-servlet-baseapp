b5de4d654584808120806a58feca998b3a9356c2
idalxdd <jubaacioly@gmail.com>
2024-11-19 14:14:54 -0300

    feature: Service atualizado para listar com paginação, contar e inserir relatorios

--- src/main/java/br/mendonca/testemaven/services/RelatorioCrescimentoService.java
+++ src/main/java/br/mendonca/testemaven/services/RelatorioCrescimentoService.java
@@ -3,6 +3,7 @@ package br.mendonca.testemaven.services;
 import br.mendonca.testemaven.dao.RelatorioCrescimentoDAO;
 import br.mendonca.testemaven.model.entities.RelatorioCrescimento;
 import br.mendonca.testemaven.services.dto.RelatorioCrescimentoDTO;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,25 +12,49 @@ public class RelatorioCrescimentoService {
 
     private RelatorioCrescimentoDAO dao = new RelatorioCrescimentoDAO();
 
-    public void criarTabela() throws ClassNotFoundException, SQLException {
-        dao.criarTabelaRelatorioCrescimento();
-    }
-
-    public void inserirRelatorio(RelatorioCrescimentoDTO dto) throws ClassNotFoundException, SQLException {
-        RelatorioCrescimento relatorio = new RelatorioCrescimento();
-        relatorio.setDataRegistro(dto.getDataRegistro());  // Alterado para int
-        relatorio.setAltura(dto.getAltura());
-        relatorio.setSaude(dto.getSaude());
-        relatorio.setObservacoes(dto.getObservacoes());
-        dao.inserirRelatorio(relatorio);
-    }
-
-    public List<RelatorioCrescimentoDTO> listarRelatorios() throws ClassNotFoundException, SQLException {
+    // Método para listar relatórios com paginação
+    public List<RelatorioCrescimentoDTO> listarRelatoriosPaginado(int pageNumber) throws ClassNotFoundException, SQLException {
+        List<RelatorioCrescimento> lista = dao.listarRelatoriosPaginado(pageNumber);
         List<RelatorioCrescimentoDTO> listaDTO = new ArrayList<>();
-        List<RelatorioCrescimento> lista = dao.listarRelatorios();
+
         for (RelatorioCrescimento relatorio : lista) {
             listaDTO.add(RelatorioCrescimentoDTO.mapper(relatorio));
         }
+
         return listaDTO;
+    }
+
+    // Método para contar o total de relatórios no banco de dados
+    public int contarTotalRelatorios() throws ClassNotFoundException, SQLException {
+        return dao.contarTotalRelatorios();
+    }
+
+    public void marcarComoInativo(int id) throws ClassNotFoundException, SQLException {
+        dao.marcarComoInativo(id);
+    }
+    
+    public List<RelatorioCrescimentoDTO> listarRelatoriosOcultos() throws ClassNotFoundException, SQLException {
+        List<RelatorioCrescimento> lista = dao.listarRelatoriosOcultos();
+        List<RelatorioCrescimentoDTO> listaDTO = new ArrayList<>();
+    
+        for (RelatorioCrescimento relatorio : lista) {
+            listaDTO.add(RelatorioCrescimentoDTO.mapper(relatorio));
+        }
+    
+        return listaDTO;
+    }
+    
+
+    // Método para inserir um novo relatório no banco de dados
+    public void inserirRelatorio(RelatorioCrescimentoDTO dto) throws ClassNotFoundException, SQLException {
+        // Converte o DTO para a entidade antes de salvar no banco
+        RelatorioCrescimento relatorio = new RelatorioCrescimento();
+        relatorio.setDataRegistro(dto.getDataRegistro());
+        relatorio.setAltura(dto.getAltura());
+        relatorio.setSaude(dto.getSaude());
+        relatorio.setObservacoes(dto.getObservacoes());
+
+        // Chama o DAO para inserir o novo relatório
+        dao.inserirRelatorio(relatorio);
     }
 }
