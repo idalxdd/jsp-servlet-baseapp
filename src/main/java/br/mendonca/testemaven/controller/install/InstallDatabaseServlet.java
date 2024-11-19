e87043d24046569bcdc0715ce4238338c11808e6
Paulo Henrique <148291338+PauloHenriqque@users.noreply.github.com>
2024-11-19 16:54:54 -0300

    refactor: modificando InstallDatabaseServlet

--- src/main/java/br/mendonca/testemaven/controller/install/InstallDatabaseServlet.java
+++ src/main/java/br/mendonca/testemaven/controller/install/InstallDatabaseServlet.java
@@ -3,6 +3,7 @@ package br.mendonca.testemaven.controller.install;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+
 import br.mendonca.testemaven.services.InstallService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,55 +13,51 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/install")
 public class InstallDatabaseServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter page = response.getWriter();
-		
-		try {
-			InstallService service = new InstallService();
-			String msg = "<h1>INSTALL DATABASE</h1>";
-			
-			service.testConnection();
-			msg += "<h2>Connection DB sucessful!</h2>\n";
-			
-			service.deleteUserTable();
-			msg += "<h2>Delete table user sucessful!</h2>\n";
-			
-			service.createUserTable();
-			msg += "<h2>Create table user sucessful!</h2>\n";
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter page = response.getWriter();
+        
+        try {
+            InstallService service = new InstallService();
+            String msg = "<h1>INSTALL DATABASE</h1>";
+            
+            service.testConnection();
+            msg += "<h2>Connection DB successful!</h2>\n";
+            
+            service.deleteUserTable();
+            msg += "<h2>Delete table user successful!</h2>\n";
+            
+            service.createUserTable();
+            msg += "<h2>Create table user successful!</h2>\n";
 
-			service.deletePlantaTable();
-			msg += "<h2>Delete table planta sucessful!</h2>\n";
+            service.deletePlantaTable();
+            msg += "<h2>Delete table planta successful!</h2>\n";
 
-			service.createPlantaTable();
-			msg += "<h2>Create table planta sucessful!</h2>\n";
-			
-			service.deleteAdocaoTable();
-			msg += "<h2>Delete table adocao sucessful!</h2>\n";
+            service.createPlantaTable();
+            msg += "<h2>Create table planta successful!</h2>\n";
 
-			service.createAdocaoTable();
-			msg += "<h2>Create table adocao sucessful!</h2>\n";
-			page.println("<html lang='pt-br'><head><title>Teste</title></head><body>");
-			page.println(msg);
-			page.println("</code>");
-			page.println("</body></html>");
-			page.close();
-			
-		} catch (Exception e) {
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
-			e.printStackTrace(pw);
-			
-			page.println("<html lang='pt-br'><head><title>Error</title></head><body>");
-			page.println("<h1>Error</h1>");
-			page.println("<code>");
-			page.println(sw.toString());
-			page.println("</code>");
-			page.println("</body></html>");
-			page.close();
-		}
-	}	
+            service.insertPlantas();
+            msg += "<h2>Insert plantas successful!</h2>\n";
+            
+            page.println("<html lang='pt-br'><head><title>Teste</title></head><body>");
+            page.println(msg);
+            page.println("</body></html>");
+            page.close();
+            
+        } catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            
+            page.println("<html lang='pt-br'><head><title>Error</title></head><body>");
+            page.println("<h1>Error</h1>");
+            page.println("<code>");
+            page.println(sw.toString());
+            page.println("</code>");
+            page.println("</body></html>");
+            page.close();
+        }
+    }   
 }
-
